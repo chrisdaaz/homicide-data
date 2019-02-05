720e04deef1289d850c0f85a9aa49ae4f9bb8ff6
Chris Diaz <chris-diaz@northwestern.edu>
2019-02-04 23:27:48 -0600

    new function, better docs, cleaner soup

--- homicide.py
+++ homicide.py
@@ -4,8 +4,8 @@ import pandas
 
 def get_soup(base_url, page):
     """
-    This returns a bunch of soup from a base url and a page. The page should
-    be a string. This does not do any preprocessing to the soup
+    This scrapes HTML from a base url and a page reference. The page should
+    be a string. This does not do any preprocessing to the HTML.
 
     ">>> get_soup("http://homicide.northwestern.edu/database/", "2")
     http://homicide.northwestern.edu/database/2
@@ -21,29 +21,30 @@ def get_soup(base_url, page):
     print(url)
     return soup
 
+def format_key(key):
+    """this formats soup into a unique key"""
+    return "{} {}".format(key.find_previous('h2').text.strip(), key.text.strip()).title().replace("'", "").replace(" ", "").replace("?", "")
+
 def case_data(soup):
 
-    """ takes soup and returns a dictionary. The dictionary is namespaced using
-    a highly opinionated idea: if there's an key or value, look above and run
-    TODO: Add a doc test if necessary.
     """
-    # Check to see if the page is an actual database record
+    This takes the scraped HTML and returns a dictionary. The dictionary is namespaced using
+    a highly opinionated idea: if there's a key or a value, look above and grab the heading and append it to the beginning of the key using titlecase (e.g. DefendantName).
+    """
     key_search ="width: 200px; text-align:left; font-family: Arial Narrow,sans-serif; font-size: 12px;"
-    value_search = "width: 450px; text-align:left;"
+    # Check to see if the page is an actual database record
     if soup.find("h2"):
+        # If so, grab the case CaseDescription
         case_desc = soup.find("h2").next_sibling.strip()
-        # Get the values for keys, namespace them by the previous H2 so that we don't end up with two of the same fields
-        keys = [ "{}-{}".format(k.find_previous('h2').text.strip(), k.text.strip()).lower().title().replace("'", "")
-                for k in soup.find_all('td', style=key_search)
-                if k.text]
-        # get the values
-        values = [v.text.strip() for v in soup.find_all('td', style=value_search) if v.text]
+        # Next, get the values for keys and namespace them to prevent duplicates
+        keys_and_values = [(format_key(k), k.find_next('td').text.strip()) for k in soup.find_all('td', style=key_search) if k.text]
         # create a list of Dictionaries
-        data = dict(zip(keys, values))
+        data = dict(keys_and_values)
         # Add case description to the case data
         data['CaseDescription'] = case_desc
         return data
-
+    else:
+        return "Not a database record"
 
 def get_all_data(number_of_items):
     """
@@ -62,28 +63,16 @@ def get_all_data(number_of_items):
     """
     base_url = "http://homicide.northwestern.edu/database/"
     return [case_data(get_soup(base_url, str(item))) for item in range(1,number_of_items)]
+
 def write_md(data, directory):
     import os
     for item in data:
-
         filename = "{}.md".format(item["CaseDescriptionCaseNumber"])
         with open(os.path.join(directory, filename), 'w') as f:
             f.write("--- \n")
             for key, value in item.items():
                 f.write("{}: {}\n".format(key, value))
             f.write("---")
-
-"""
-OR
-
-            for key, value in item.items():
-                f.write("{}: "{}"\n".format(key, value))
-            f.write("\n---")
-
-if the above doesn't work:
-    Ah. There's also something called Yaml pipe style that might do it:
-    key: | This is my "goofy" 'string'
-"""
 
 def main(number_of_items, csv_file, output_dir_for_md):
 
@@ -96,6 +85,9 @@ def main(number_of_items, csv_file, output_dir_for_md):
     number_of_items --> number of data pages you want to process from homicide
     csv_file --> the output of the csv data file
     output_dir_for_md --> the output directory for the .md files
+
+    Example:
+    >>> homicide.main(50, "/mnt/c/Users/cpd3149/Desktop/output.csv", "/mnt/c/Users/cpd3149/Desktop/")
     """
     data = [d for d in get_all_data(number_of_items) if d]
     write_md(data, output_dir_for_md)
